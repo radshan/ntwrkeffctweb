3f0950d26d44d0914295d112178a172e2873d764
radshan <39998988+radshan@users.noreply.github.com>
2021-08-05 21:19:33 +0530

    Update interface.js

--- public/interface.js
+++ public/interface.js
@@ -45,6 +45,66 @@ const links = {
     linkURL: "https://www.notboring.co/p/the-great-online-game",
     linkDesc: "As soon as you connect to the Internet, you play the Great Online Game."
   },
+  
+  link8: {
+    linkName: "Advice for ambitious 19 yo",
+    linkURL: "https://blog.samaltman.com/advice-for-ambitious-19-year-olds",
+    linkDesc: "Sam Altman's advice to ambitious 19 year olds."
+  },
+  
+  link9: {
+    linkName: "Modesty kills excellence",
+    linkURL: "https://limitlesscuriosity.com/essays/modesty-kills-excellence",
+    linkDesc: "By making modesty a virtue, we have made excellence a vice."
+  },
+  
+  link10: {
+    linkName: "Why does DARPA work?",
+    linkURL: "https://benjaminreinhardt.com/wddw",
+    linkDesc: "Excellent breakdown of DARPA's structure."
+  },
+  
+  link11: {
+    linkName: "The Law of Accelerating Returns",
+    linkURL: "https://www.kurzweilai.net/the-law-of-accelerating-returns",
+    linkDesc: "Quite a long read on exponential returns."
+  },
+  
+  link12: {
+    linkName: "The Wild World of High Frequency Stock Trading",
+    linkURL: "https://youtu.be/aMhI70KUAzw",
+    linkDesc: "An 30 minute long video covering the nitty gritties of high frequency trading."
+  },
+  
+  link13: {
+    linkName: "How do Airplanes fly?",
+    linkURL: "https://youtu.be/F077WDnB8P8",
+    linkDesc: "YouTube video covering the basic physics behind airplanes."
+  },
+  
+  link14: {
+    linkName: "The ABCs of DAOs",
+    linkURL: "https://1729.com/daos",
+    linkDesc: "Decentralized Autonomous Organisations. Quite a mouthful, am I right? Read about them in this 8 minute long article."
+  },
+  
+  link15: {
+    linkName: "The History of TikTok",
+    linkURL: "https://www.acquired.fm/episodes/tiktok",
+    linkDesc: "A podcast episode that covers the history of one of the greatest tech start-ups of our era: ByteDance."
+  },
+  
+  link16: {
+    linkName: "Alias.co",
+    linkURL: "https://alias.co/",
+    linkDesc: "Keep a track of content posted by our generation's greatest minds."
+  },
+  
+  link17: {
+    linkName: "Ethereum: Into the Ether",
+    linkURL: "https://open.spotify.com/episode/5ZXzLELW1489aCjUPHjTO4?si=cSw9xDxiR1WkoglJm09ANw&dl_branch=1&nd=1",
+    linkDesc: "Amazing podcast episode on Ethereum."
+  },
 };
 
 const divMain = document.getElementById("div-main");
